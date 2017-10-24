edb7dfbbf0e78310d60f513827ab5f5a436df8cb
Yohan Lasorsa <yohanlasorsa.thales@gmail.com>
2017-10-24 16:03:33 +0200

    Updated from generator

--- src/app/core/shell/shell.component.spec.ts
+++ src/app/core/shell/shell.component.spec.ts
@@ -1,12 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TranslateModule } from '@ngx-translate/core';
 
 import { AuthenticationService } from '../authentication/authentication.service';
 import { MockAuthenticationService } from '../authentication/authentication.service.mock';
 import { ShellComponent } from './shell.component';
 import { CoreModule } from '../core.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('ShellComponent', () => {
   let component: ShellComponent;
