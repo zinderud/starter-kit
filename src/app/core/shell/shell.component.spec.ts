3e4018709db196b245f3ae620ec28f47a2c6caa6
Yohan Lasorsa <yohanlasorsa.thales@gmail.com>
2017-10-24 14:04:11 +0200

    Fixed lint/test errors

--- src/app/core/shell/shell.component.spec.ts
+++ src/app/core/shell/shell.component.spec.ts
@@ -1,5 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TranslateModule } from '@ngx-translate/core';
 
 import { AuthenticationService } from '../authentication/authentication.service';
@@ -16,6 +17,7 @@ describe('ShellComponent', () => {
       imports: [
         RouterTestingModule,
         TranslateModule.forRoot(),
+        BrowserAnimationsModule,
         CoreModule
       ],
       providers: [
